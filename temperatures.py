d35e4c55336995da29784ea2aa0c07005f94d40d
RithyruthThouMorng <rithyruth.thoumorng@my.jcu.edu.au>
2019-11-29 09:16:41 +0800

    Add functions to main program

--- temperatures.py
+++ temperatures.py
@@ -2,26 +2,41 @@
 CP1404/CP5632 - Practical
 Pseudocode for temperature conversion
 """
-
-MENU = """C - Convert Celsius to Fahrenheit
+def main():
+    MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print("Result: {:.2f} F".format(fahrenheit))
-    elif choice == "F":
-        # TODO: Write this section to convert F to C and display the result
-        fahrenheit = float(input("Farenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
-        print("Result: {:.2f} C".format(celsius))
-        # Hint: celsius = 5 / 9 * (fahrenheit - 32)
-        # Remove the "pass" statement when you are done. It's a placeholder.
-    else:
-        print("Invalid option")
     print(MENU)
     choice = input(">>> ").upper()
-print("Thank you.")
+
+
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = convert_c_to_f(celsius)
+            print("Result: {:.2f} F".format(fahrenheit))
+        elif choice == "F":
+            # TODO: Write this section to convert F to C and display the result
+            fahrenheit = float(input("Farenheit: "))
+            celsius = convert_f_to_c(fahrenheit)
+            print("Result: {:.2f} C".format(celsius))
+        # Hint: celsius = 5 / 9 * (fahrenheit - 32)
+        # Remove the "pass" statement when you are done. It's a placeholder.
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def convert_c_to_f(celsius):
+    """Converts Celsius to Fahrenheit"""
+    return celsius * 9.0 / 5 + 32
+
+
+def convert_f_to_c(fahrenheit):
+    """Converts Celsius to Fahrenheit"""
+    return 5 / 9 * (fahrenheit - 32)
+
+
+main()
