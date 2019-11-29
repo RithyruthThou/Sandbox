9536e643f0cb354947cf77a224a279bf866f86f8
RithyruthThouMorng <rithyruth.thoumorng@my.jcu.edu.au>
2019-11-29 09:05:56 +0800

    Change to password_check.py

--- password_check.py
+++ password_check.py
@@ -1,10 +1,20 @@
-valid = False
-while not valid:
-    password = input("Please enter your password")
-    if len(password) <6:
-        valid = False
-    else:
-        valid = True
-        for char in range (0, len(password)):
-            print("*", end=' ')
+def main():
+    password = get_password()
+    password_check(password)
 
+def get_password():
+    secret = input("Please enter your password")
+    return secret
+
+def password_check(secret):
+    valid = False
+    while not valid:
+        if len(secret) <6:
+            secret = input("Please enter your password")
+            valid = False
+        else:
+            valid = True
+            for char in range (0, len(secret)):
+                print("*", end =' ')
+
+main()
